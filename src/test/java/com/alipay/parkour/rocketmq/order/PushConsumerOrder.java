82539a1405a4781bdd63362580f74a82c47bed9e
recollects <recollects@aliyun.com>
2018-05-07 23:14:55 +0800

    gts test

--- src/test/java/com/alipay/parkour/rocketmq/order/PushConsumerOrder.java
+++ src/test/java/com/alipay/parkour/rocketmq/order/PushConsumerOrder.java
@@ -1,37 +1,40 @@
 package com.alipay.parkour.rocketmq.order;
 
-import java.util.List;
-
 import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
 import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import com.alibaba.rocketmq.client.exception.MQClientException;
 import com.alibaba.rocketmq.common.consumer.ConsumeFromWhere;
 import com.alibaba.rocketmq.common.message.MessageExt;
 
+import java.util.List;
+
 /**
- * @author jiadong
- * @date 2018/4/25 14:31
+ * @author recollects
+ * @version V1.0
+ * @date 2018年04月24日 下午10:38
  */
 public class PushConsumerOrder {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws InterruptedException, MQClientException {
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("CID_YE_1");
-        consumer.setNamesrvAddr("127.0.0.1:9876");
-        consumer.subscribe("TOPIC_YE_1", "*");
+        consumer.setNamesrvAddr("localhost:9876");
+        consumer.subscribe("TOPIC_YE_1", "TAG_YE_1");
         consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
-        consumer.setConsumeTimestamp(System.currentTimeMillis() + "");
+        //wrong time format 2017_0422_221800
+        consumer.setConsumeTimestamp("20170422221800");
         consumer.registerMessageListener(new MessageListenerConcurrently() {
 
             @Override
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
-                System.out.println(Thread.currentThread().getName() + msgs);
-                throw new RuntimeException("模拟重发...");
-
-                //return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+                System.out.println(Thread.currentThread().getName()+"----"+msgs);
+                throw new RuntimeException("模拟异常....");
+//                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             }
         });
-
         consumer.start();
         System.out.printf("Consumer Started.%n");
     }
+
 }
